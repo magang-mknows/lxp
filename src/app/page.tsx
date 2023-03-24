bef61c6b0666b0b8cdc9a6fe2a5c401fd5000e32
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-03-24 16:47:26 +0700

    buxgifix: error build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import LandingModule from "@/modules/landing";
+import { NextPage } from "next";
+import { ReactElement } from "react";
 
 export const metadata = {
   title: "LXP App",
   description: "LXP App adalah LMS berbasis AI anjy mabar bosqu",
 };
 
-export default async function Home() {
+const Home: NextPage = (): ReactElement => {
   return <LandingModule />;
-}
+};
+
+export default Home;
