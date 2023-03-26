95e92b3592099d8c9f593180a0fdba14d583541b
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-03-27 00:05:25 +0700

    feature:about page

--- src/utils/const/index.tsx
+++ src/utils/const/index.tsx
@@ -127,7 +127,7 @@ export const AboutJobList = [
   },
   {
     title: "Outbond",
-    srcImg: "/assets/about/outbond.webp",
+    srcImg: "/assets/about/Outbond.webp",
   },
   {
     title: "Events",
