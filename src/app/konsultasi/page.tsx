4c58ee97e325cac042a4f01c1f7002c368d5cb22
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-03-27 23:53:17 +0700

    feature: add document service

--- src/app/konsultasi/page.tsx
+++
@@ -1,9 +0,0 @@
-import ConsultingModule from "@/modules/consulting-service/ConsultingAndServiceModule";
-import { NextPage } from "next";
-import React, { ReactElement } from "react";
-
-const ConsltingPage: NextPage = (): ReactElement => {
-  return <ConsultingModule />;
-};
-
-export default ConsltingPage;
