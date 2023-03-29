eb918ad9da0a17ddd2deb7a3b38a4cd1a9a8fdf1
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-03-29 15:08:53 +0700

    feature : career consulting page

--- src/app/konsultasi-layanan/konsultasi-karir/page.tsx
+++ src/app/konsultasi-layanan/konsultasi-karir/page.tsx
@@ -1,6 +1,9 @@
 import CareerConsultingModule from "@/modules/consulting-service/consulting/career/CareerConsultingModule";
 import React from "react";
 
+export const metadata = {
+  title: "Konsultasi Karir | LXP App",
+};
 const page = () => {
   return <CareerConsultingModule />;
 };
