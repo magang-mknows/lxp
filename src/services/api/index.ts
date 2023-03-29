14829d2d8c593c7ca57c577eb9b486b277e03ede
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-03-29 17:03:56 +0700

    feature:skeleton loading document process page

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 import TokenService from "@/services/token";
-import AuthService from "@/services/auth";
 
 const ApiService = {
   _401interceptor: null || 0,
@@ -21,35 +20,34 @@ const ApiService = {
     return axios(data);
   },
 
-  mount401Interceptor() {
-    this._401interceptor = axios.interceptors.response.use(
-      (response) => {
-        return response;
-      },
-      async (error) => {
-        if (error.request.status === 401) {
-          if (error.config.url.includes("auth/login")) {
-            AuthService.Logout();
-            throw error;
-          } else {
-            try {
-              const res = await AuthService.RefreshToken();
-              TokenService.saveRefreshToken(res?.data?.refresh_token);
-              TokenService.saveToken(res?.data?.access_token);
-            } catch (e) {
-              AuthService.Logout();
-              throw error;
-            }
-          }
-        }
-        throw error;
-      },
-    );
-  },
+  // mount401Interceptor() {
+  //   this._401interceptor = axios.interceptors.response.use(
+  //     (response) => {
+  //       return response;
+  //     },
+  //     async (error) => {
+  //       if (error.request.status === 401) {
+  //         if (error.config.url.includes("auth/login")) {
+  //           throw error;
+  //         } else {
+  //           try {
+  //             const res = await AuthService.RefreshToken();
+  //             TokenService.saveRefreshToken(res?.data?.refresh_token);
+  //             TokenService.saveToken(res?.data?.access_token);
+  //           } catch (e) {
+  //             AuthService.Logout();
+  //             throw error;
+  //           }
+  //         }
+  //       }
+  //       throw error;
+  //     },
+  //   );
+  // },
 
-  unmount401Interceptor() {
-    axios.interceptors.response.eject(this._401interceptor);
-  },
+  // unmount401Interceptor() {
+  //   axios.interceptors.response.eject(this._401interceptor);
+  // },
 };
 
 export default ApiService;
