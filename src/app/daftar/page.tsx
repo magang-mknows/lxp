d21957378685ba899fc973906d24516103df234f
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-03-29 13:28:26 +0700

    improvement : init page register

---
+++ src/app/daftar/page.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import React, { ReactElement } from "react";
+
+const RegisterPage: NextPage = (): ReactElement => {
+  return <div>page</div>;
+};
+
+export default RegisterPage;
