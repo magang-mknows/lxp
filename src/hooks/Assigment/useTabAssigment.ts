72fb8a210bf59ac1fbc20039ef90cdfbcd83385c
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-03-31 14:45:47 +0700

    improvement : create custom hooks assigment page

---
+++ src/hooks/Assigment/useTabAssigment.ts
@@ -0,0 +1,12 @@
+import { useRecoilState } from "recoil";
+import { tabAssigmentState } from "@/store/Assigment";
+
+type ReturnTypes = { getTabAssigment: string; setTabAssigment: (val: string) => void };
+
+export const useTabAssigment = (): ReturnTypes => {
+  const [get, set] = useRecoilState(tabAssigmentState);
+  return {
+    getTabAssigment: get,
+    setTabAssigment: (val: string) => set(val),
+  };
+};
