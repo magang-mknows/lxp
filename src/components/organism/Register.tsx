377cc259ad306149e5c75d8c54f9fdcd01af2fdf
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-04-03 14:42:39 +0700

    improvement : add guide module

--- src/components/organism/Register.tsx
+++ src/components/organism/Register.tsx
@@ -1,14 +1,15 @@
 import React, { Suspense } from "react";
 import RegisterForm from "../moleculs/RegisterForm";
 import SplashRegister from "../moleculs/SplashRegister";
+import SplashRegisterLoading from "../moleculs/SplashRegisterLoading";
 
 const RegisterPage = () => {
   return (
     <div className="lg:flex">
-      <Suspense>
+      <Suspense fallback={<SplashRegisterLoading />}>
         <SplashRegister />
       </Suspense>
-      <Suspense>
+      <Suspense fallback={<RegisterForm />}>
         <RegisterForm />
       </Suspense>
     </div>
