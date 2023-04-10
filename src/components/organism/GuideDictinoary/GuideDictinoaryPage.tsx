9cbdcb144f7a2c4a11bb0449ccd431990bc0b1af
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-04-10 13:59:40 +0700

    improvement : page dictinoary

---
+++ src/components/organism/GuideDictinoary/GuideDictinoaryPage.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React, { ReactElement, Suspense } from "react";
+import LayoutProvider from "@/providers/LayoutProvider";
+import Breadcums from "@/components/atoms/Breadcums";
+import { guideDictinoaryBreadCumbs } from "@/utils/const";
+import dynamic from "next/dynamic";
+
+const Sidebar = dynamic(() => import("./sidebar"));
+const ContentSection = dynamic(() => import("./content"));
+
+const GuideDictionaryPage = (): ReactElement => {
+  return (
+    <div>
+      <LayoutProvider className="!bg-[#FAFAFA]">
+        <Suspense>
+          <Breadcums items={guideDictinoaryBreadCumbs} />
+          <div className=" w-full flex flex-col-reverse lg:flex-row justify-center lg:gap-[90px]">
+            <Sidebar />
+            <ContentSection />
+          </div>
+        </Suspense>
+      </LayoutProvider>
+    </div>
+  );
+};
+
+export default GuideDictionaryPage;
---
+++ src/modules/Guide/guide-dictionary/GuideDictinoaryModule.tsx
@@ -0,0 +1,8 @@
+import GuideDictionaryPage from "@/components/organism/GuideDictinoary/GuideDictinoaryPage";
+import React, { ReactElement } from "react";
+
+const GuideDictinoaryModule = (): ReactElement => {
+  return <GuideDictionaryPage />;
+};
+
+export default GuideDictinoaryModule;
