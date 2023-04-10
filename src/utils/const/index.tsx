d7bb7ae87f78fb85be083833dd35400dd636a6c1
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-04-10 14:06:47 +0700

    improvement : add breadcumb

--- src/utils/const/index.tsx
+++ src/utils/const/index.tsx
@@ -124,7 +124,26 @@ export const discussionRoomBreadCumbs = [
   { name: "Home", link: "/" },
   { name: "Ruang Diskusi", link: "/ruang-diskusi" },
 ];
+export const guideDictinoaryBreadCumbs = [
+  { name: "Home", link: "/" },
+  { name: "Panduan", link: "/panduan" },
+  { name: "Kamus LMS MKnows", link: "/panduan/video-panduan/kamus" },
+];
 
+export const leaderBoardBreadCumbs = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "Dashboard",
+    link: "/dashboard",
+  },
+  {
+    name: "Papan Skor",
+    link: "/papan-skor",
+  },
+];
 export const FeaturesList = [
   {
     title: "Administrasi",
