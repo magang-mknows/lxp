84072fba5fe7a7c388fc170f42f432fe34bae2cc
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-04-12 15:04:12 +0700

    fix conflict

--- src/components/atoms/types.d.ts
+++ src/components/atoms/types.d.ts
@@ -172,3 +172,15 @@ export type TDiscussinCardProps = {
   type: string;
   countLikes: number;
 };
+
+export interface PopupProfilProps {
+  id?: string;
+  img: string;
+  score: number;
+  lookup?: boolean;
+  name: string;
+  index?: string | number;
+  stylePopup?: string;
+  widthModal?: string;
+  onClose?: MouseEventHandler<HTMLDivElement | HTMLSpanElement>;
+}
