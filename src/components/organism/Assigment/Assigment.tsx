d46309110991a71f4e4fa3d0508515021a3f4d06
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-04-16 22:33:52 +0700

    fix:build error

--- src/components/organism/Assigment/Assigment.tsx
+++ src/components/organism/Assigment/Assigment.tsx
@@ -2,7 +2,7 @@ import React, { Fragment, ReactElement, Suspense } from "react";
 import AssigmentStatus from "./AssigmentStatus";
 import BannerAssigmentSection from "./BannerAssigmentSection";
 import BannerLoading from "@/modules/Assigment/BannerLoading";
-import AssigmentPageLoading from "@/modules/Assigment/loading";
+import AssigmentPageLoading from "@/modules/Assigment/AssigmentPageLoading";
 
 const AssigmentPage = (): ReactElement => {
   return (
