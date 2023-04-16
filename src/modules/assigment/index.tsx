7c6eca68a98f080149a2eb65aefd584bfc80631f
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-04-16 22:46:51 +0700

    fix:build error

--- src/modules/assigment/index.tsx
+++ src/modules/assigment/index.tsx
@@ -2,7 +2,7 @@ import React, { Fragment, ReactElement, Suspense } from "react";
 import LayoutProvider from "@/providers/LayoutProvider";
 import AssigmentStatus from "@/components/organism/Assigment/AssigmentStatus";
 import BannerAssigmentSection from "@/components/organism/Assigment/BannerAssigmentSection";
-import AssigmentPageLoading from "./AssigmentPageLoading";
+import AssigmentPageLoading from "./loading";
 import BannerLoading from "./BannerLoading";
 
 const AssigmentPage = (): ReactElement => {
