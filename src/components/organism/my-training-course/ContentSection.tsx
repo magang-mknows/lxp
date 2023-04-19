d3d5ba423fadada17aa15e11674cffff60f28996
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-04-19 22:09:51 +0700

    improvement : fix src img

--- src/components/organism/my-training-course/ContentSection.tsx
+++ src/components/organism/my-training-course/ContentSection.tsx
@@ -1,10 +1,6 @@
 import { FC, Fragment, ReactElement } from "react";
 import Image from "next/image";
 import { MdArrowDropDown } from "react-icons/md";
-import imgQuizIcon from "/public/assets/course/quiz-icon.svg";
-import imgAssignmentIcon from "/public/assets/course/assignment-icon.svg";
-import imgDiscussionIcon from "/public/assets/course/discussion-icon.svg";
-import imgDoneIcon from "/public/assets/course/done-icon.svg";
 import Link from "next/link";
 
 const ContentSection: FC = (): ReactElement => {
@@ -38,49 +34,91 @@ const ContentSection: FC = (): ReactElement => {
                 </Link>
               </div>
               <div className="">
-                <Image src={imgDoneIcon} alt="" />
+                <Image
+                  src={"/assets/course/done-icon.svg"}
+                  className="w-auto"
+                  width={100}
+                  height={100}
+                  alt=""
+                />
               </div>
             </div>
             <div className="h-[60px] items-center flex justify-between border-b border-[#D4D4D4] pl-[21px] pr-[40.5px]">
               <div className="flex gap-x-6">
-                <Image src={imgQuizIcon} alt="" />
+                <Image
+                  src={"/assets/course/quiz-icon.svg"}
+                  className="w-auto"
+                  width={100}
+                  height={100}
+                  alt=""
+                />
                 <Link
-                  href="/studi-ku/course/quiz"
+                  href="/"
                   className="text-base text-neutral-800 dark:text-neutral-400 hover:underline"
                 >
                   Quiz
                 </Link>
               </div>
               <div className="">
-                <Image src={imgDoneIcon} alt="" />
+                <Image
+                  src={"/assets/course/done-icon.svg"}
+                  className="w-auto"
+                  width={100}
+                  height={100}
+                  alt=""
+                />
               </div>
             </div>
             <div className="h-[60px] items-center flex justify-between border-b border-[#D4D4D4] pl-[21px] pr-[40.5px]">
               <div className="flex gap-x-6">
-                <Image src={imgAssignmentIcon} alt="" />
+                <Image
+                  src={"/assets/course/assignment-icon.svg"}
+                  className="w-auto"
+                  width={100}
+                  height={100}
+                  alt=""
+                />
                 <Link
-                  href="/studi-ku/course/penugasan-test"
+                  href="/"
                   className="text-base text-neutral-800 dark:text-neutral-400 hover:underline"
                 >
                   Tugas
                 </Link>
               </div>
               <div className="">
-                <Image src={imgDoneIcon} alt="" />
+                <Image
+                  src={"/assets/course/done-icon.svg"}
+                  className="w-auto"
+                  width={100}
+                  height={100}
+                  alt=""
+                />
               </div>
             </div>
             <div className="h-[60px] items-center flex justify-between border-b border-[#D4D4D4] pl-[21px] pr-[40.5px]">
               <div className="flex gap-x-6">
-                <Image src={imgDiscussionIcon} alt="" />
+                <Image
+                  src={"/assets/course/discussion-icon.svg"}
+                  className="w-auto"
+                  width={100}
+                  height={100}
+                  alt=""
+                />
                 <Link
-                  href="/studi-ku/course/diskusi"
+                  href="/"
                   className="text-base text-neutral-800 dark:text-neutral-400 hover:underline"
                 >
                   Diskusi
                 </Link>
               </div>
               <div className="">
-                <Image src={imgDoneIcon} alt="" />
+                <Image
+                  src={"/assets/course/done-icon.svg"}
+                  className="w-auto"
+                  width={100}
+                  height={100}
+                  alt=""
+                />
               </div>
             </div>
           </div>
